02d5c659c58b9746660c2913fa9e758a654014fe
Vandana <vandanapandey0404@gmail.com>
2025-03-11 18:17:30 +0530

    Update Signup.jsx
    code signup

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -28,7 +28,7 @@ const Signup = () => {
 
     
     try {
-        const url = "http://localhost:8080/auth/signup";
+        const url = `https://deploy-mern-app-1-api.vercel.app/auth/signup`;
         const response = await fetch(url, {
             method: "POST",
             headers: {
